25935d51d29689093440c6f413ad55bbf6c2525d
saberbin <saberbin0921@gmail.com>
2019-10-22 23:46:53 +0800

    添加bing图片爬虫

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # small_spider
 small_spider
+
+
+
+## 使用方法
+
+`bing_img_spider_v2.py`为必应背景图片爬虫。
+
+环境：`python3.7 + requests + BeautifulSoup`
+
+执行方法：
+
+在命令行运行：
+
+`python bing_img_spider_v2.py file_name`或者`python3 bing_img_spider_v2.py file_name`
+
+例如：`python bing_img_spider_v2.py aaa `
+
+会在当前脚本的`img`文件夹保存`aaa.jpg`的图片。
---
+++ small_spider/bing_img_spider_v2.py
@@ -0,0 +1,60 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+import time
+import sys
+
+
+HEADERS = (
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36',
+        'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20120101 Firefox/33.0',
+        'Mozilla/5.0 (MSIE 10.0; Windows NT 6.1; Trident/5.0)',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A'
+        )
+
+
+def get_content(url):
+    global HEADERS
+    try:
+        r = requests.get(url)
+        return r
+    except Exception as e:
+        print(e)
+        return None
+
+def main(file_name):
+
+    base_url = 'https://cn.bing.com'
+
+    # response  = requests.get(base_url)
+    response = get_content(base_url)
+    if response is None:
+        print("The response is None.")
+        return
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    #提取源代码中 data-ultra-definition-src 的属性值，然后拼贴到一起
+
+    pic_path = soup.find('div', attrs={'id': 'bgImgProgLoad'})['data-ultra-definition-src']
+    print(pic_path)
+    pic_url = base_url + pic_path
+
+    # urlretrieve(url=pic_url, filename=r'/Users/linxiaoyue/Desktop/today_wallpaper.jpg')
+    # file_path = "D:\\project\\vs_code_project\\small_spider\\img\\"
+    # "D:\\project\\vs_code_project\\small_spider\\small_spider\\img\\"
+    # file_name = file_path + str(int(time.time())) + '_wallpaper.jpg'
+    file_path = '.\\img\\'
+    if not file_name:
+        file_name = '%6d'%random.randint(10000, 999999)
+    img_content = get_content(pic_url).content
+    with open(file_path + file_name + '.jpg', 'wb') as f:
+        f.write(img_content)
+
+
+if __name__ == '__main__':
+    file_name = sys.argv[1]
+    main(file_name)
+    print('end')
